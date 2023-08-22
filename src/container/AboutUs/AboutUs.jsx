45169f8a5b8a542b1ee2bb0ce5690f2f7a8d4afb
Chanatinart K <chanatinart.k@gmail.com>
2023-08-22 15:07:24 +0700

    about us section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    {/* G LETTER */}
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g letter" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      {/* ABOUT US */}
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about-spoon" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button className="custom__button">Know More</button>
+      </div>
+      {/* KNIFE IMG */}
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="knife" />
+      </div>
+      {/*OUR HISTORY */}
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about-spoon" />
+        <p className="p__opensans">
+          Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat.
+          Risus placerat morbi volutpat habitasse interdum mi aliquam In sed
+          odio nec aliquet.
+        </p>
+        <button className="custom__button">Know More</button>
+      </div>
+    </div>
   </div>
 );
 
