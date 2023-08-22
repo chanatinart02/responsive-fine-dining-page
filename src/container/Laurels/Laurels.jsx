15a55e1ca0c50ef150b87080bbe114bfdb9a92ae
Chanatinart K <chanatinart.k@gmail.com>
2023-08-22 21:01:30 +0700

    add Awards section

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import './Laurels.css';
+import { SubHeading, MenuItem } from "../../components";
+import { images, data } from "../../constants";
+import "./Laurels.css";
+
+//  takes an award object as a prop
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt={title} />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{ color: "#dcca87" }}>
+        {title}
+      </p>
+      <p className="p__cormorant">{subtitle}</p>
+    </div>
+  </div>
+);
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+
+      {/* Awards */}
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => (
+          <AwardCard award={award} key={award.title} />
+        ))}
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels" />
+    </div>
   </div>
 );
 
