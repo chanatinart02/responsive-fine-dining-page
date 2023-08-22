dd3c65300ece6d5234b3d046a75a828b03bdef7a
Chanatinart K <chanatinart.k@gmail.com>
2023-08-22 18:10:40 +0700

    add special menu section

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app__menuItem">
+    <div className="app__menuItem-head">
+      {/* Menu name */}
+      <div className="app__menuItem-name">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {title}
+        </p>
+      </div>
+
+      <div className="app__menuItem-dash"></div>
+
+      {/* Price Menu */}
+      <div className="app__menuItem-price">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {price}
+        </p>
+      </div>
+    </div>
+
+    {/* Ingredients */}
+    <div className="app__menuItem-sub">
+      <p className="p__opensans" style={{ color: "#AAA" }}>
+        {tags}
+      </p>
+    </div>
   </div>
 );
 
